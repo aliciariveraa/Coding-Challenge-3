4ee9c358a11e8f69628329bbd3486e39bae9b137
aliciariveraa <aliciarivera@usf.edu>
2025-01-31 00:28:57 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,8 @@
+// Task 1: Product Price Management
+let prices = [45, 49, 12, 144, 55]; // Declare an array with at least 5 prices
+// Add a new price to the array
+prices.push(23);
+// Remove the first price from the array
+prices.shift();
+// Log the updated array to the console
+console.log('Updated Price List:', prices);
